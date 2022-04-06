d027088730a7824ea24f9509da1b1302f4468f9c
Juan5987 <juan.machado.5987@gmail.com>
2022-04-06 12:00:19 +0200

    added width in the dependency array of useEffect

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -30,7 +30,7 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
     return () => {
       window.removeEventListener('resize', listener);
     };
-  });
+  }, [width]);
 
   if (direction === 'horizontal') {
     resizableProps = {
